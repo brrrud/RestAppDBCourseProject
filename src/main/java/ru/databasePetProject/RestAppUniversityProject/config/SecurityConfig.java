711cf913663c5ac3285ba336263b454791d0702e
brrrud <106093165+brrrud@users.noreply.github.com>
2023-12-19 20:06:59 +0300

    develop init

--- src/main/java/ru/databasePetProject/RestAppUniversityProject/config/SecurityConfig.java
+++ src/main/java/ru/databasePetProject/RestAppUniversityProject/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package ru.databasePetProject.RestAppUniversityProject.config;
 
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
 import ru.databasePetProject.RestAppUniversityProject.security.AuthProvider;
+
+import java.util.List;
+
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -18,8 +27,13 @@ public class SecurityConfig {
     public SecurityConfig(AuthProvider authProvider) {
         this.authProvider = authProvider;
     }
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+    }
+
 
     protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) {
         authenticationManagerBuilder.authenticationProvider(authProvider);
     }
+
 }
